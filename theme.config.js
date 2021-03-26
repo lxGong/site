9b47e134c4f48194439e70f4423a4ca1aa3d3fce
lxGong <58767596+lxGong@users.noreply.github.com>
2021-03-26 09:22:11 +0800

    Update theme.config.js

--- theme.config.js
+++ theme.config.js
@@ -3,7 +3,7 @@ const YEAR = (new Date).getFullYear()
 export default {
   footer: <small style={{ display: 'block', marginTop: '8rem' }}>
     <abbr title='This site and all its content are licensed under a Creative Commons Attribution-NonCommercial 4.0 International License.' style={{cursor: 'help'}}>CC BY-NC 4.0</abbr> <time>{YEAR}</time> © GONG.
-
+    <a href="/feed.xml">RSS</a>
     <style jsx>{`
       a {
         float: right;
